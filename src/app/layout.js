6886061a7ad9b89c352f10737828d8df5196396c
Mohirdev <89441169+Mohirdevelopers@users.noreply.github.com>
2023-08-05 15:05:59 +0500

    feat: style issue fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import "./globals.css";
 import "../../public/assets/libs/bootstrap/css/bootstrap.min.css";
 import "../../public/assets/libs/line-awesome/css/line-awesome.min.css";
 import "animate.css";
@@ -6,7 +7,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "../../public/assets/css/style.css";
-import "./globals.css";
 import { LocaleContextProvider } from "@/context/locale.context";
 
 const inter = Inter({ subsets: ["latin"] });
