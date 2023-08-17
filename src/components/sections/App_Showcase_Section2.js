28bb234eb919700b9bbbb3f6578da123ee59dd47
Ulugbek <ulugbek4work@gmail.com>
2023-08-17 06:58:27 +0500

    migrated statistics section

--- src/components/sections/App_Showcase_Section2.js
+++ src/components/sections/App_Showcase_Section2.js
@@ -1,125 +1,57 @@
 "use client";
 
+import { useLocaleContext } from "../../context/locale.context";
+
 export default function App_Showcase_Section2() {
+  const { translate } = useLocaleContext();
   return (
     <>
       <section className="section spdt">
         <div className="container">
           <div className="heading align-center">
-            <div className="heading-sub color-navy">Why using our app</div>
             <h2 className="heading-title size-l">
-              Our app is great for individuals, <br /> startups and enterprises
+              {translate("statistics.paragraph")}
             </h2>
           </div>
-          <div className="row flex-align-c">
-            <div className="col-lg-3">
-              <div className="block-icon-box layout-05">
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-umbrella" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">High usability</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-bell" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">Action Reminder</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-file-alt" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">Merge Files</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
+          <div className="block-number">
+            <div className="row">
+              <div className="col-lg-4 col-md-8">
+                <div className="item">
+                  <h4 className="title">{translate("statistics.first_col")}</h4>
+                  <div className="number">
+                    <span style={{ marginRight: "12px" }} className="first">
+                      18
+                    </span>
+                    <span className="first">
+                      {translate("common.million_short")}
+                    </span>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="images layout-02 lg-mt60 lg-mb60">
-                <div className="inner">
-                  <img
-                    className="img01"
-                    src="/assets/images/asc3.png"
-                    alt="Image"
-                  />
-                  <img
-                    className="img02 wow animate__animated      animate__fadeInLeft"
-                    src="/assets/images/product-images/2.webp"
-                    alt="Image"
-                  />
-                  <img
-                    className="img03 wow animate__animated      animate__fadeInRight"
-                    src="/assets/images/asc5.png"
-                    alt="Image"
-                  />
-                  <img
-                    className="img04 lg-hidden"
-                    src="/assets/images/asc6.png"
-                    alt="Image"
-                  />
+
+              <div className="col-lg-4 col-md-8">
+                <div className="item">
+                  <h4 className="title">{translate("statistics.sec_col")}</h4>
+                  <div className="number">
+                    <span style={{ marginRight: "12px" }} className="first">
+                      740
+                    </span>
+                    <span className="first">
+                      {translate("common.million_short")}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-3">
-              <div className="block-icon-box layout-05">
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-fill-drip" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">Free Live Chat</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-leaf" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">Social Share</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="icon-box">
-                  <div className="inner">
-                    <div className="icon">
-                      <i className="las la-shield-alt" />
-                    </div>
-                    <div className="content">
-                      <h3 className="title">Custom Shortcuts</h3>
-                      <div className="desc">
-                        It is a long established fact that a reader
-                      </div>
-                    </div>
+              <div className="col-lg-4 col-md-8">
+                <div className="item">
+                  <h4 className="title">{translate("statistics.third_col")}</h4>
+                  <div className="number">
+                    <span style={{ marginRight: "12px" }} className="first">
+                      18
+                    </span>
+                    <span className="first">
+                      {translate("common.thausand_short")}
+                    </span>
                   </div>
                 </div>
               </div>
