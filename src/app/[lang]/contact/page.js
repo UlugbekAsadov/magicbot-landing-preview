b86babfaabcd875d48b6774c8ba0ea44b28009c4
Ulugbek laptop <ulugb3k270@gmail.com>
2023-08-17 18:25:35 +0500

    integrated backend on lead form

--- src/app/[lang]/contact/page.js
+++ src/app/[lang]/contact/page.js
@@ -1,20 +1,55 @@
 "use client";
 import Layout from "@/components/layout/Layout";
-import Link from "next/link";
+import { useLocaleContext } from "@/context/locale.context";
 import { useState } from "react";
 export default function ContactOne() {
+  const [hasSubmitted, setHasSubmitForm] = useState(
+    sessionStorage.getItem("isLeadSubmitted")
+  );
   const [formValues, setFormValues] = useState({
     full_name: "",
     phone_number: "+998",
     shop_name: "",
   });
 
+  const [hasError, setHasError] = useState(false);
+  const { translate } = useLocaleContext();
+
   const handleChangeValue = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value });
   };
 
-  const handleSubmitform = () => {
-    console.log(formValues);
+  const handleSubmitform = (e) => {
+    e.preventDefault();
+
+    if (formValues.phone_number !== 13) {
+      return setHasError(true);
+    }
+
+    try {
+      const body = {
+        name: formValues.full_name,
+        phone: formValues.phone_number,
+        shop: formValues.shop_name,
+      };
+
+      const config = {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      fetch(process.env.NEXT_PUBLIC_API + "/lead", config)
+        .then((res) => res.json())
+        .then((data) => {
+          sessionStorage.setItem("isLeadSubmitted", true);
+          setHasSubmitForm(true);
+        });
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
@@ -74,7 +109,7 @@ export default function ContactOne() {
                 ></iframe>
               </div>
             </div>
-            <div className="col-lg-6">
+            <div className="col-lg-6 relative">
               <div className="contact-form layout-01">
                 <div className="heading">
                   <h2 className="heading-title">Хотите подключиться?</h2>
@@ -96,6 +131,7 @@ export default function ContactOne() {
                           className="px-3 "
                           value={formValues.full_name}
                           onChange={handleChangeValue}
+                          required
                         />
                       </div>
                     </div>
@@ -107,6 +143,7 @@ export default function ContactOne() {
                           name="phone_number"
                           id="phone_number"
                           maxLength={13}
+                          minLength={13}
                           placeholder="Введите свое номер телефона"
                           className="px-3 "
                           value={formValues.phone_number}
@@ -116,7 +153,13 @@ export default function ContactOne() {
                               event.preventDefault();
                             }
                           }}
+                          required
                         />
+                        {hasError && (
+                          <span className="text-xs text-red-500">
+                            Validation error
+                          </span>
+                        )}
                       </div>
                     </div>
                     <div className="col-md-12">
@@ -130,6 +173,7 @@ export default function ContactOne() {
                           className="px-3 "
                           value={formValues.shop_name}
                           onChange={handleChangeValue}
+                          required
                         />
                       </div>
                     </div>
@@ -146,6 +190,18 @@ export default function ContactOne() {
                   </div>
                 </form>
               </div>
+              {hasSubmitted && (
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center  backdrop-blur-lg">
+                  <div className="bg-white animate-submittionPopup flex flex-col items-center justify-center max-w-5xl w-full rounded-xl mx-5 px-5 py-8 md:py-14">
+                    <h2 className="font-bold text-3xl md:text-4xl  gradient-text text-center">
+                      {translate("form.thanks")}
+                    </h2>
+                    <p className="font-medium max-w-md w-full mt-3 md:mt-9 text-xl md:text-2xl text-center text-zinc-400">
+                      {translate("form.message")}
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
