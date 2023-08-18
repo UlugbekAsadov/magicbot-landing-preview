37b3971aae5bb75c823b7da39b76ca769f368579
Ulugbek laptop <ulugb3k270@gmail.com>
2023-08-18 16:21:23 +0500

    bugfix: fixed dark mode issues

--- src/components/elements/SwitchDarkLight.js
+++ src/components/elements/SwitchDarkLight.js
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
 
 export default function SwitchDarkLight() {
-  const [colorMode, setColorMode] = useState(localStorage.getItem("theme"));
+  const [colorMode, setColorMode] = useState(
+    localStorage.getItem("theme") || "dark",
+  );
   useEffect(() => {
-    document.body.classList.toggle("sala-dark-scheme", colorMode === "light");
+    document.body.classList.toggle("sala-dark-scheme", colorMode === "dark");
   }, []);
 
   const handleChangeTheme = (theme) => {
-    document.body.classList.toggle("sala-dark-scheme", theme === "light");
+    document.body.classList.toggle("sala-dark-scheme", theme === "dark");
     setColorMode(theme);
     localStorage.setItem("theme", theme);
   };
 
   return (
     <div className="flex items-center">
-      {colorMode === "light" ? (
-        <button onClick={handleChangeTheme.bind(null, "dark")}>
+      {colorMode === "dark" ? (
+        <button onClick={handleChangeTheme.bind(null, "light")}>
           <img src="./assets/icons/sun-icon.svg" alt="sun-icon" />
         </button>
       ) : (
-        <button onClick={handleChangeTheme.bind(null, "light")}>
+        <button onClick={handleChangeTheme.bind(null, "dark")}>
           <img src="./assets/icons/moon-icon.svg" alt="sun-icon" />
         </button>
       )}
