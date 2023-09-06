3eef5dd3e3666345d4efcfe5f019775ef7511aba
Ulugbek laptop <ulugb3k270@gmail.com>
2023-09-06 11:28:15 +0500

    feat: seo improved

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -5,22 +5,22 @@ import { LocaleContextProvider } from "@/context/locale.context";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Magicbot by Humocommerce",
-  description:
-    "Превратите свой Telegram-бот в полноценный онлайн-магазин с Magicstore",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <title>{"Magicbot"}</title>
+        <meta
+          name="description"
+          content="Превратите свой Telegram-бот в полноценный онлайн-магазин
+            с Magicstore"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
+
       <body className={inter.className}>
         <LocaleContextProvider>{children}</LocaleContextProvider>
       </body>
