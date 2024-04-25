f9942a27294472a7d0ae5ae075f59af558363bd1
Ulugbek <ulugb3k270@gmail.com>
2024-04-25 18:37:27 +0500

    feat: added scripts

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -37,6 +37,52 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+         (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+         m[i].l=1*new Date();
+         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+         ym(97130109, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+         });`,
+          }}
+        ></script>
+
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/97130109"
+              style="position:absolute; left:-9999px;"
+              alt=""
+            />
+          </div>
+        </noscript>
+
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1914552095649289');
+              fbq('track', 'PageView');
+              `,
+          }}
+        />
       </head>
 
       <body className={inter.className}>
