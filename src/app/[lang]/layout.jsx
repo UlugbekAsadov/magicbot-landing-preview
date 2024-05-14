69f0324891f9d7f2d3382349f97a27eeebc5c50c
Ulugbek Asadov <80874328+UlugbekAsadov@users.noreply.github.com>
2024-05-14 11:13:59 +0500

    Update layout.jsx

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -19,6 +19,11 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
           rel="stylesheet"
         />
+
+          <meta
+          name="facebook-domain-verification"
+          content="u1q50wffgw3uky83uq0i29gb8ohog5"
+        />
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-CY54FHWERY"
@@ -37,6 +42,31 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+
+               <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1585615572223846');
+        fbq('track', 'PageView');
+       `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=1585615572223846&ev=PageView&noscript=1"
+          />
+        </noscript>
 
         <script
           type="text/javascript"
