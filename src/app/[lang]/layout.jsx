709e7f964c58a8a9597d62a50c3b511f26882191
o2b3k <humowels@gmail.com>
2024-05-17 12:27:42 +0500

    Refactor layout.jsx for cleaner code
    The layout.jsx file has been refactored to enhance code readability. Unnecessary lines and spaces have been removed. Also, the script tags have been rearranged and assigned proper IDs for better code maintenance.

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -1,38 +1,27 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import Script from "next/script";
 import "./globals.scss";
-import { LocaleContextProvider } from "@/context/locale.context";
+import {LocaleContextProvider} from "@/context/locale.context";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="uz">
-      <head>
-        <title>{"Magicbot"}</title>
-        <meta
-          name="description"
-          content="Превратите свой Telegram-бот в полноценный онлайн-магазин
-            с Magicstore"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
-          rel="stylesheet"
-        />
-
-          <meta
-          name="facebook-domain-verification"
-          content="u1q50wffgw3uky83uq0i29gb8ohog5"
-        />
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-CY54FHWERY"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+    <head>
+      <title>{"Magicbot"}</title>
+      <meta name="description" content="Превратите свой Telegram-бот в полноценный онлайн-магазин с Magicstore"/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet"/>
+      <meta name="facebook-domain-verification" content="u1q50wffgw3uky83uq0i29gb8ohog5"/>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-CY54FHWERY"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -40,12 +29,13 @@ export default function RootLayout({ children }) {
                 page_path: window.location.pathname,
               });
             `,
-          }}
-        />
+        }}
+      />
 
-               <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Script
+        id="facebook-metrics"
+        dangerouslySetInnerHTML={{
+          __html: `
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -57,21 +47,21 @@ export default function RootLayout({ children }) {
         fbq('init', '1585615572223846');
         fbq('track', 'PageView');
        `,
-          }}
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style="display:none"
+          src="https://www.facebook.com/tr?id=1585615572223846&ev=PageView&noscript=1"
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style="display:none"
-            src="https://www.facebook.com/tr?id=1585615572223846&ev=PageView&noscript=1"
-          />
-        </noscript>
+      </noscript>
 
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Script
+        id="yandex-metrics"
+        dangerouslySetInnerHTML={{
+          __html: `
          (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
          m[i].l=1*new Date();
          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -84,24 +74,27 @@ export default function RootLayout({ children }) {
               accurateTrackBounce:true,
               webvisor:true
          });`,
-          }}
-        ></script>
-
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/97130109"
-              style="position:absolute; left:-9999px;"
-              alt=""
-            />
-          </div>
-        </noscript>
-
-      </head>
-
-      <body className={inter.className}>
-        <LocaleContextProvider>{children}</LocaleContextProvider>
-      </body>
+        }}/>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/97130109"
+            style="position:absolute; left:-9999px;"
+            alt=""
+          />
+        </div>
+      </noscript>
+      <Script id="gtag-metrics"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3SDK5VG');`,
+        }}/>
+    </head>
+    <body className={inter.className}>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3SDK5VG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <LocaleContextProvider>{children}</LocaleContextProvider>
+    </body>
     </html>
   );
 }
