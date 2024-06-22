26ea5f1b3485c7d5d6452ec1e58ccb84ebb60bbc
Ulugbek <ulugb3k270@gmail.com>
2024-06-22 19:16:33 +0500

    magicbot clients sections added

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -15,6 +15,7 @@ import SellSection from "@/components/sections/sell-section";
 import OurLeadsSection from "@/components/sections/our-leads-section";
 import { Footer } from "@/components/sections/footer";
 import { Blogs } from "@/components/sections/blogs/blogs-section";
+import { MagicbotClientsSertion } from "@/components/sections/magicbot-clients-section";
 
 export default function HomeAppShowcase({ params }) {
   const { lang } = params;
@@ -27,11 +28,12 @@ export default function HomeAppShowcase({ params }) {
       >
         <HeroSection />
         <StatisticsSection />
-        <SellSection />
-        <MarketSection />
+        <MagicbotClientsSertion />
+        {/* <SellSection /> */}
+        {/* <MarketSection /> */}
         <MarketingSection />
         <InterfaceSection />
-        <AiSection />
+        {/* <AiSection /> */}
         <CrmSection />
         <IntegrationSection />
         <GuideSection />
--- src/components/sections/blogs/blog.jsx
+++ src/components/sections/blogs/blog.jsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export const Blog = ({ blog }) => {
-
-
   return (
-    <Link aria-label={blog.title} href={`/blogs/${blog.slug}`} className="col-4">
+    <Link
+      aria-label={blog.title}
+      href={`/blogs/${blog.slug}`}
+      className="col-4"
+    >
       {blog.image && (
         <img
           className="rounded-top w-100 h-48 object-cover"
@@ -17,9 +18,7 @@ export const Blog = ({ blog }) => {
         />
       )}
       <div className="background-grey rounded-bottom p-3 p-md-4">
-        <h2 className=" fw-bold fs-6 text-truncate">
-          {blog.title}
-        </h2>
+        <h2 className=" fw-bold fs-6 text-truncate">{blog.title}</h2>
         <div
           className="text-secondary fs-6 mt-2 fw-normal text-truncate"
           dangerouslySetInnerHTML={{ __html: blog.description }}
