5fc3af99a8531d9a54cde79a8806e35195a46aa1
o2b3k <humowels@gmail.com>
2024-06-28 18:28:28 +0500

    Update header right section layout
    The layout of the header right section has been updated in both layout and new-layout directories. The SVG icon button has been replaced with a new button that contains an icon and a free demo text.

--- src/components/layout/Header/header-right-section.jsx
+++ src/components/layout/Header/header-right-section.jsx
@@ -1,28 +1,20 @@
 import Link from "next/link";
 import { useLocaleContext } from "@/context/locale.context";
-import { useUtmContext } from "@/context/utm.context";
+import {useUtmContext} from "@/context/utm.context";
 
 export default function HeaderRightSection({ lang }) {
   const { translate } = useLocaleContext();
   const { link } = useUtmContext();
   return (
-    <div className=" flex flex-align-center flex-content-end">
-      <Link href={link} className="w-12 h-12 bg-blue-600 rounded-md flex items-center justify-center text-white">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-7"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
-          />
-        </svg>
-      </Link>
-    </div>
+    <>
+      <div className=" flex flex-align-c flex-content-e">
+        <div className="buttons">
+          <Link href={link} className="button fullfield">
+            <i className="las la-headset" />
+            <span>{translate("header.free_demo")}</span>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
